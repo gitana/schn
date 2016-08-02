d2e04423512034b563bc942ed42eb261d9d9434b
Harry Moore <harrymoore@yahoo.com>
2016-08-02 07:58:58 -0400

    split import data into 15 batches to limit the size of each upload. loop over the batches to package, upload, import

--- redeploy-content.sh
+++ redeploy-content.sh
@@ -2,10 +2,21 @@
 
 # uninstall anything existing
 # we do this twice because some things don't resolve the first time
-cloudcms uninstall
-cloudcms uninstall
+# cloudcms uninstall
+# cloudcms uninstall
 
-# packages the archive, uploads and imports content into branch
-cloudcms package schn-import app 1
-cloudcms upload schn-import app 1
-cloudcms import schn-import app 1
+count=0
+for dir in custom/docs/import/CloudCMS\ database*/
+do
+    dir=${dir%*/}
+    echo ***********************
+    echo ${dir} ****************
+    count=`expr $count + 1`    
+    cd custom
+    node ./import.js -t schn:article -f "../${dir}" -n ./docs/import/attachments/unzipped -m /articles -k ./build
+
+    cd ..
+    cloudcms package schn-import-batch-${count} app 1
+    cloudcms upload schn-import-batch-${count} app 1
+    cloudcms import schn-import-batch-${count} app 1
+done
