52c4c3c23ea21a405f2a529e40044391929ee366
Harry Moore <harrymoore@yahoo.com>
2016-08-21 07:57:57 -0500

    remove Articles folder on uninstall

--- setup/uninstall.js
+++ setup/uninstall.js
@@ -2,6 +2,7 @@ module.exports = function(installer, callback)
 {
     installer.removeContentInstances("schn:article");
     installer.removeContentAtPath("/Article Documents");
+    installer.removeContentAtPath("/Articles");
     installer.execute(function(err, results) {
         callback(err, results);
     });
