a4284678bff1596c2f251b5f6caed669021770fa
Harry Moore <harrymoore@yahoo.com>
2016-09-09 14:42:21 -0400

    Splash screen module

---
+++ ui-extensions/schn-splash/index.js
@@ -0,0 +1,29 @@
+define(function(require) {
+
+    require("./login-splash/login-splash");
+
+    var $ = require("jquery");
+    var OneTeam = require("oneteam");
+
+    $(document).on("dispatch", function(event, ratchet, completed) {
+
+        if (completed)
+        {
+            if (!ratchet.isDetached())
+            {
+                if (window.location.hash === "#/")
+                {
+                    var config = {};
+                    config.title = "Welcome to Cloud CMS";
+                    config.cancel = false;
+                    config.close = true;
+
+                    OneTeam.showOverlay("trial-splash", config, function (err) {
+                        //alert("DONE");
+                    });
+                }
+            }
+        }
+    });
+
+});
