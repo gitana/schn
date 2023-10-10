1a2961916c266ea4b9ffc9ec8be08d6263e049d9
mwhitman <mwhitman95@gmail.com>
2023-10-10 15:15:45 -0400

    debug tweaks

--- ui-extensions/schn-sort/actions/default/request-attachment.js
+++ ui-extensions/schn-sort/actions/default/request-attachment.js
@@ -4,16 +4,24 @@
 define(function(require, exports, module) {
     var Ratchet = require("ratchet/ratchet");
 
-    return Ratchet.Actions.register("request-attachment", Ratchet.AbstractAction.extend({
+    return Ratchet.Actions.register("request-attachment", Ratchet.AbstractUIAction.extend({
 
         defaultConfiguration: function()
         {
+            console.log("configure!");
             var config = this.base();
 
             config.title = "Request Attachment";
             config.iconClass = "fad fa-download";
 
             return config;
+        },
+
+        execute: function(config, actionContext, callback)
+        {
+            this.doAction(actionContext, function(err, result) {
+                callback(err, result);
+            });
         },
 
         doAction: function(actionContext, callback)
