3f5d5a4924d9fbc5f425e2c52fbce4aa8c1b54ac
mwhitman <mwhitman95@gmail.com>
2023-10-19 11:16:17 -0400

    send email in request-attachment

--- ui-extensions/schn-sort/actions/default/request-attachment.js
+++ ui-extensions/schn-sort/actions/default/request-attachment.js
@@ -1,6 +1,7 @@
 define(function(require, exports, module) {
     var AbstractRibbonTool = require("ribbon/abstract-ribbon-tool");
     var RibbonToolRegistry = require("ribbon/ribbon-tool-registry");
+    var OneTeam = require("oneteam");
 
     console.log("register request attachment");
 
@@ -26,6 +27,55 @@ define(function(require, exports, module) {
             });
 
             finished();
+        },
+
+        execute: function(config, actionContext, callback)
+        {
+            var self = this;
+            var emailId = config.emailId;
+
+            console.log("Execute action");
+
+            OneTeam.oneTeamApplication(self, function() {
+
+                var application = this;
+
+                var publicEmailProviderId = null;
+                if (application.public)
+                {
+                    publicEmailProviderId = application.public.emailProviderId;
+                }
+
+                if (publicEmailProviderId)
+                {
+                    var emailModel = {
+                        "username": "myguy",
+                        "node": {
+                            "title": "Heyhey"
+                        }
+                    };
+                    
+                    console.log("Send email");
+
+                    $.ajax({
+                        "type": "POST",
+                        "contentType": "application/json",
+                        "dataType": "json",
+                        "processData": false,
+                        "url": `/proxy/applications/${application.getId()}/emailprovider/send?id=${emailId}`,
+                        "data": JSON.stringify(emailModel),
+                        "headers": {
+                            "X-CSRF-TOKEN": OneTeam.getCsrfToken()
+                        }
+                    }).done(function(data) {
+    
+                        callback();
+    
+                    }).fail(function(xhr) {
+                        OneTeam.errorHandler(xhr);
+                    }); 
+                }
+            });
         }
     }));
 });
