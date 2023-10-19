06f9045e9bd5c67aa6371fd22dd999020761bee5
mwhitman <mwhitman95@gmail.com>
2023-10-19 13:01:48 -0400

    add blocking modal

--- ui-extensions/schn-sort/actions/default/request-attachment.js
+++ ui-extensions/schn-sort/actions/default/request-attachment.js
@@ -2,6 +2,7 @@ define(function(require, exports, module) {
     var AbstractRibbonTool = require("ribbon/abstract-ribbon-tool");
     var RibbonToolRegistry = require("ribbon/ribbon-tool-registry");
     var OneTeam = require("oneteam");
+    var Ratchet = require("ratchet/ratchet");
 
     console.log("register request attachment");
 
@@ -20,48 +21,50 @@ define(function(require, exports, module) {
                 "align": "left",
                 "clickHandler": function(ribbon) {
                     return function() {
-                        console.log("Request attachment...");
-
-                        OneTeam.oneTeamApplication(ribbon, function() {
-
-                            var application = this;
-                            var document = ribbon.observable("document").get();
-                            var user = ribbon.observable("user").get();
-                            var emailTemplateId = tool.config.emailTemplateId;
-
-            
-                            var publicEmailProviderId = null;
-                            if (application.public)
-                            {
-                                publicEmailProviderId = application.public.emailProviderId;
-                            }
-            
-                            if (publicEmailProviderId)
-                            {
-                                Chain(application).readEmailProvider(publicEmailProviderId).then(function() {
-
-                                    var emailProvider = this;
-                                    var from = emailProvider.from;
-
-                                    var emailModel = {
-                                        "user": user,
-                                        "node": document
-                                    };
-
-                                    // create email
-                                    application.createEmail({
-                                        "to": "michael.whitman@gitana.io",
-                                        "from": from,
-                                        "subject": "Attachment Request",
-                                        "bodyRepositoryId": document.getRepositoryId(),
-                                        "bodyBranchId": document.getBranchId(),
-                                        "bodyNodeId": emailTemplateId
-                                    }).then(function() {
-                                        console.log("Send email");
-                                        this.subchain(emailProvider).send(this, emailModel)
+                        Ratchet.block("Requesting Attachment...", function() {
+                            OneTeam.oneTeamApplication(ribbon, function() {
+    
+                                var application = this;
+                                var document = ribbon.observable("document").get();
+                                var user = ribbon.observable("user").get();
+                                var emailTemplateId = tool.config.emailTemplateId;
+    
+                
+                                var publicEmailProviderId = null;
+                                if (application.public)
+                                {
+                                    publicEmailProviderId = application.public.emailProviderId;
+                                }
+                
+                                if (publicEmailProviderId)
+                                {
+                                    Chain(application).readEmailProvider(publicEmailProviderId).then(function() {
+    
+                                        var emailProvider = this;
+                                        var from = emailProvider.from;
+    
+                                        var emailModel = {
+                                            "user": user,
+                                            "node": document
+                                        };
+    
+                                        // create email
+                                        application.createEmail({
+                                            "to": "michael.whitman@gitana.io",
+                                            "from": from,
+                                            "subject": "Attachment Request",
+                                            "bodyRepositoryId": document.getRepositoryId(),
+                                            "bodyBranchId": document.getBranchId(),
+                                            "bodyNodeId": emailTemplateId
+                                        }).then(function() {
+                                            this.subchain(emailProvider).send(this, emailModel).then(function() {
+                                                console.log("Send email");
+                                                Ratchet.unblock();
+                                            })
+                                        });
                                     });
-                                });
-                            }
+                                }
+                        });
                         });
                         return false;
                     }
